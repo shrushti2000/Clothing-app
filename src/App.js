7637a32db86aaf5de322d8c539769986fb3a9b81
shrushti2000 <shrushtipolekar@gmail.com>
2020-06-11 15:25:50 +0530

    Created homepage and added routing

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage=()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
+      
+    </div>
+  );
+}
+
+export default App;
