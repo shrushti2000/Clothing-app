f892aca275eccad728ccc5abf92c7c9fa14189a1
shrushti2000 <shrushtipolekar@gmail.com>
2020-06-13 19:05:29 +0530

    added sign in component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 
 
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/signin' component={SignInAndSignPage}/>
       </Switch>
       
     </div>
