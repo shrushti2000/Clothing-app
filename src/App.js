cba34f3712cf551858cf21ff149302de16b49231
shrushti2000 <shrushtipolekar@gmail.com>
2020-06-21 16:02:40 +0530

    implemented redux in react,created user-reducer, and made modifications in sign-in page using redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route,Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,26 +10,22 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
-
+import {setCurrentUser} from'./redux/user/user.actions';
 
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      currentUser:null
-    };
-  }
+  
 
   unsubscribeFromAuth=null;
 
   componentDidMount(){
+    const {setCurrentUser}=this.props;
     this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth=>{
       if(userAuth){
         const userRef=await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot=>{
-          this.setState({
+          setCurrentUser({
             currentUser:{
               id:snapShot.id,
               ...snapShot.data()
@@ -36,7 +33,7 @@ class App extends React.Component {
           });
         })
       }
-      this.setState({currentUser:userAuth});
+      setCurrentUser(userAuth);
     })
   }
   componentWillUnmount(){
@@ -47,11 +44,11 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header  currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' component={SignInAndSignPage}/>
+          <Route exact path='/signin' render={()=>this.props.currentUser ? (<Redirect to='/'/>):(<SignInAndSignPage/>)}/>
         </Switch>
         
       </div>
@@ -60,4 +57,12 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapStateToProps=({user})=>({
+  currentUser:user.currentUser
+});
+
+const mapDispatchtoProps=dispatch=>({
+  setCurrentUser:user=>dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps,mapDispatchtoProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+
+import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assests/crown.svg';
 import './header.style.scss';
 import {auth} from '../../firebase/firebase.utils.js';
@@ -18,4 +20,9 @@ const Header =({currentUser})=>(
     </div>
 
 );
-export default Header;
+
+const mapStateToProps=state=>({
+        currentUser:state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
